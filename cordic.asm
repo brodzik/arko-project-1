0cd34f29af7cadc00e8059d2f3dd559bca70afa3
Adrian Brodzik <adrianbrodzyk@gmail.com>
2019-11-09 22:35:17 +0100

    Fix bugs

--- cordic.asm
+++ cordic.asm
@@ -1,10 +1,9 @@
 	.data
-NEW_LINE: .asciiz "\n"
-INV_K:	.word 0x26DD3B6A
+K_INV:	.word 652032874
 SCALE:	.word 1073741824
 N_TAB:	.byte 32
-CTAB:	.word 0x3243F6A8, 0x1DAC6705, 0x0FADBAFC, 0x07F56EA6, 0x03FEAB76, 0x01FFD55B, 0x00FFFAAA, 0x007FFF55, 0x003FFFEA, 0x001FFFFD, 0x000FFFFF, 0x0007FFFF, 0x0003FFFF, 0x0001FFFF, 0x0000FFFF, 0x00007FFF, 0x00003FFF, 0x00001FFF, 0x00000FFF, 0x000007FF, 0x000003FF, 0x000001FF, 0x000000FF, 0x0000007F, 0x0000003F, 0x0000001F, 0x0000000F, 0x00000008, 0x00000004, 0x00000002, 0x00000001, 0x00000000
-THETA:	.word 1
+CTAB:	.word 843314856, 497837829, 263043836, 133525158, 67021686, 33543515, 16775850, 8388437, 4194282, 2097149, 1048575, 524287, 262143, 131071, 65535, 32767, 16383, 8191, 4095, 2047, 1023, 511, 255, 127, 63, 31, 15, 8, 4, 2, 1, 0
+THETA:	.word 1686629713
 	.text
 	.globl main
 main:
@@ -12,46 +11,32 @@ main:
 	lb	$t1, N_TAB		# $t1 = N_TAB
 	la	$t2, CTAB		# $t2 = CTAB + i
 	
-	lw	$t3, INV_K		# $t3 = x
+	lw	$t3, K_INV		# $t3 = x
 	li	$t4, 0			# $t4 = y
 	lw	$t5, THETA		# $t5 = z
 loop:
 	beq	$t0, $t1, end
 	
-	li	$s0, 0			# d = sign of z
-	bgtz	$t5, pos
-	li	$s0, -1
-pos:
+	srav	$s1, $t4, $t0		# $s1 = y >> i
+	srav	$s2, $t3, $t0		# $s2 = x >> i
+	lw	$s3, ($t2)		# $s3 = arctan(2^-i)
 	
-	srlv	$s1, $t4, $t0
-	xor	$s1, $s1, $s0
-	sub	$s1, $s1, $s0
-	sub	$s1, $t3, $s1		# $s1 = tx
-	
-	srlv	$s2, $t3, $t0
-	xor	$s2, $s2, $s0
-	sub	$s2, $s2, $s0
-	add	$s2, $s2, $t4		# $s2 = ty
-	
-	lw	$s3, ($t2)
-	xor	$s3, $s3, $s0
-	sub	$s3, $s3, $s0
-	sub	$s3, $t5, $s3		# $s3 = tz
-	
-	move	$t3, $s1		# x = tx
-	move	$t4, $s2		# y = ty
-	move	$t5, $s3		# z = tz
-	
+	bltz	$t5, sign_neg
+	sub	$t3, $t3, $s1		# x = x - (y >> i)
+	add	$t4, $t4, $s2		# y = y + (x >> i)
+	sub	$t5, $t5, $s3		# z = z - arctan(2^-i)
+	j	sign_end
+sign_neg:
+	add	$t3, $t3, $s1		# x = x + (y >> i)
+	sub	$t4, $t4, $s2		# y = y - (x >> i)
+	add	$t5, $t5, $s3		# z = z + arctan(2^-i)
+sign_end:
 	addi	$t0, $t0, 1
 	addi	$t2, $t2, 4
 	j	loop
 end:
 	li	$v0, 1
 	move	$a0, $t4
-	syscall
-	
-	li	$v0, 4
-	la	$a0, NEW_LINE
 	syscall
 	
 	li	$v0, 10			# exit
